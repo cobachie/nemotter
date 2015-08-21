565cb051b912c9f004e11960b27415bd40b8402d
arutoraisu <crayon6393@gmail.com>
2015-08-21 09:25:12 +0900

    Woops!

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150820062438) do
+ActiveRecord::Schema.define(version: 20150820075013) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.string   "tag"
+    t.integer  "user_id"
+    t.integer  "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
+    t.string   "remark"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,24 +47,5 @@ ActiveRecord::Schema.define(version: 20150820062438) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-=======
-ActiveRecord::Schema.define(version: 20150820075013) do
-
-  create_table "messages", force: :cascade do |t|
-    t.text     "body"
-    t.string   "tag"
-    t.integer  "user_id"
-    t.integer  "team_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "teams", force: :cascade do |t|
-    t.string   "name"
-    t.string   "remark"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
->>>>>>> 1ff8f01b55292b2449b326bce0c56138a97da668
 
 end
